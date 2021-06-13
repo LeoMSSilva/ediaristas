92750cc9ffb9a2863a2b210561e10484ba8d6a23
LeoMSSilva <l.eom1994@hotmail.com>
2021-06-13 17:27:58 -0300

    Footer created

--- src/ui/components/surfaces/Footer/index.tsx
+++ src/ui/components/surfaces/Footer/index.tsx
@@ -1,10 +1,36 @@
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
-import { FooterStyle } from './style';
+import { AppList, FooterContainer, FooterStyle, FooterTitle } from './style';
 
 const Footer: React.FC = () => {
 	return (
 		<FooterStyle>
-			Footer
+			<FooterContainer>
+				<Box sx={{ maxWidth: '25rem' }}>
+					<FooterTitle>Quem somos</FooterTitle>
+					<Typography variant={'body2'} sx={{ mt: 2 }}>
+						E-Diaristas te ajuda a encontrar um profissional perfeito para
+						realizar a limpeza da sua casa. Garantimos a melhor profissional com
+						total segurança e praticidade! São milhares de clientes satisfeitos
+						por todo o país.
+					</Typography>
+				</Box>
+				<div>
+					<FooterTitle>Baixe nossos aplicativos</FooterTitle>
+					<AppList>
+						<li>
+							<a href={'/'} target={'_blank'}>
+								<img src="/img/logos/app-store.png" alt="app-store" />
+							</a>
+						</li>
+						<li>
+							<a href={'/'} target={'_blank'}>
+								<img src="/img/logos/google-play.png" alt="google-play" />
+							</a>
+						</li>
+					</AppList>
+				</div>
+			</FooterContainer>
 		</FooterStyle>
 	);
 };
--- src/ui/components/surfaces/Footer/style.tsx
+++ src/ui/components/surfaces/Footer/style.tsx
@@ -1,3 +1,4 @@
+import { Container, Typography } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 
 export const FooterStyle = styled('footer')`
@@ -6,4 +7,31 @@ export const FooterStyle = styled('footer')`
 		theme.palette.getContrastText(theme.palette.primary.main)};
 	padding: ${({ theme }) => theme.spacing(4)} 0;
 	margin-top: auto;
+`;
+
+export const FooterContainer = styled(Container)`
+	display: flex;
+	justify-content: space-between;
+	gap: ${({ theme }) => theme.spacing(2)};
+	flex-wrap: wrap;
+	${({ theme }) => theme.breakpoints.down('md')}{
+		gap: ${({ theme }) => theme.spacing(5)};
+	}
+`;
+
+export const FooterTitle = styled((props) => (
+	<Typography component={'h2'} variant={'body2'} {...props} />
+))`
+	font-weight: bold;
+`;
+
+export const AppList = styled('ul')`
+	display: flex;
+	list-style: none;
+	padding: 0;
+	gap: ${({ theme }) => theme.spacing()};
+
+	img {
+		width: 7.625rem;
+	}
 `;
